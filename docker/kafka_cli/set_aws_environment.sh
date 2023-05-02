4896456bad188cbae0ce9d2f30d20abb9ff647c9
A.J. Bobo <aj.bobo@niceincontact.com>
2023-05-02 13:18:34 -0600

    Updated set_aws_environment.sh so that it gets a session token if the profile doesn't have one already

--- docker/kafka_cli/set_aws_environment.sh
+++ docker/kafka_cli/set_aws_environment.sh
@@ -8,4 +8,13 @@ export AWS_ACCESS_KEY_ID=$(jq -r '.AccessKeyId' creds.json)
 export AWS_SECRET_ACCESS_KEY=$(jq -r '.SecretAccessKey' creds.json)
 export AWS_SESSION_TOKEN=$(jq -r '.SessionToken' creds.json)
 
+if [ $AWS_SESSION_TOKEN = "null" ]
+then
+  echo "Need to get a session token"
+  aws sts get-session-token --profile $aws_profile > creds.json
+  export AWS_ACCESS_KEY_ID=$(jq -r '.Credentials.AccessKeyId' creds.json)
+  export AWS_SECRET_ACCESS_KEY=$(jq -r '.Credentials.SecretAccessKey' creds.json)
+  export AWS_SESSION_TOKEN=$(jq -r '.Credentials.SessionToken' creds.json)
+fi
+
 echo "Environment variables set for profile $aws_profile"
